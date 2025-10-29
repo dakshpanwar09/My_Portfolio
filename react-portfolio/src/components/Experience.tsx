d8b61652b30be3ad970ee50b0419e905e90e8078
dakshpanwar09 <152015397+dakshpanwar09@users.noreply.github.com>
2025-10-29 20:03:54 +0530

    Update experience section with new role details

--- react-portfolio/src/components/Experience.tsx
+++ react-portfolio/src/components/Experience.tsx
@@ -26,13 +26,23 @@ const Experience: React.FC = () => {
           transition={{ delay: 0.3, duration: 0.6 }}
           viewport={{ once: true }}
         >
-          <h3>Software Engineer Intern</h3>
-          <p>TechShinry</p>
-          <p>Sept 2024 - May 2025</p>
+          <h3>Software Development Trainee</h3>
+          <p>TechShiney, Greater Noida West, Uttar Pradesh</p>
+          <p>07 Sep 2024 - 30 Apr 2025</p>
           <ul>
-            <li>Worked on real-world projects integrating Salesforce and MuleSoft to streamline business operations.</li>
-            <li>Salesforce: Customized CRM, created custom objects, workflows, auto-triggered business processes. Developed Apex triggers and dashboards.</li>
-            <li>MuleSoft: Designed and implemented APIs. Conducted data transformations using DataWeave and ensured data flow integrity.</li>
+            <li>Collaborated on real-world projects integrating Salesforce and MuleSoft to streamline business operations.</li>
+            <li>Customized Salesforce CRM by creating custom objects, workflows, and automated Apex triggers, and developed interactive dashboards.</li>
+            <li>Designed MuleSoft APIs, performed data transformations using DataWeave, and maintained data flow integrity across systems.</li>
+          </ul>
+          <br />
+          
+          <h3>Software Development Intern</h3>
+          <p>Kodnest Technologies, Bengaluru, Karnataka, India</p>
+          <p>23 May 2025 - 29 Oct 2025</p>
+          <ul>
+            <li>Gained practical expertise during an internship at KodNest by working with Java, MySQL, and Frontend Technology, as well as performing Manual Testing.</li>
+            <li>Actively contributed to live projects, demonstrating strong problem-solving skills.</li>
+            <li>Learning industry standards and collaborating with technology professionals.</li>
           </ul>
         </motion.div>
       </div>
